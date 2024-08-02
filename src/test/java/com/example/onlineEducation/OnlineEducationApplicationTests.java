bcafa19951273dce3f46613fabd549d255cbe825
seulseul's <seulseul@MacBook-Pro.local>
2024-08-02 11:35:24 +0900

    Initial commit

---
+++ src/test/java/com/example/onlineEducation/OnlineEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
