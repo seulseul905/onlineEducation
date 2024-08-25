bed5735414a305dbc52af04d097ed023c6e4cf97
zemba-lee <zemba.lee@kakaostyle.com>
2024-08-25 10:32:19 +0900

    Package 구조 변경으로 BasePackages 경로 변경

--- src/main/java/com/edu/onlineEducation/OnlineEducationApplication.java
+++ src/main/java/com/edu/onlineEducation/OnlineEducationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.*")
+@ComponentScan(basePackages = "com.edu.*")
 public class OnlineEducationApplication {
 
     public static void main(String[] args) {
